19ce05b9b863bb7d55baf969af6d73cc95205613
NicholasPiano <nicholas.d.piano@gmail.com>
2015-09-26 23:13:43 +0100

    forking to dev profile

--- rowbot/woot/apps/event/models.py
+++ rowbot/woot/apps/event/models.py
@@ -10,3 +10,41 @@ from django.db import models
 
 
 ### Models
+class Event(models.Model):
+  '''
+  The type of an object with a subclass can be determined by testing with: hasattr(class, 'subclass_var_name') -> bool
+  e.g. hasattr(event, 'outing')
+
+  The Event class defines a general model for a recurring event. Events are by nature recurring, but the EventInstance
+  class describes a single, non-recurring element of its existance. I have CellInstance to thank for this. This principle
+  can also be applied to a person fulfilling a role; a "MemberInstance".
+
+  '''
+
+  # connections
+
+  # properties
+
+class Outing(Event):
+  # connections
+
+  # properties
+  pass
+
+class Race(Event):
+  # connections
+
+  # properties
+  pass
+
+class TrainingSession(Event):
+  # connections
+
+  # properties
+  pass
+
+class EventInstance(models.Model):
+  # connections
+
+  # properties
+  pass
