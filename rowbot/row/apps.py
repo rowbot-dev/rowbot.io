e83c007e2f6c76b9d3ba8fb1ea049f336e3eb624
Nicholas Piano <nicholas.d.piano@gmail.com>
2017-12-19 18:56:49 +0000

    Remove old files

--- rowbot/row/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class RowConfig(AppConfig):
-  name = 'row'
--- rowbot/row/models/__init__.py
+++
@@ -1,8 +0,0 @@
-
-# Local
-from row.models.asset import AssetCategory, AssetType, Asset, AssetInstance
-from row.models.club import Club
-from row.models.event import EventCategory, EventType, Event, EventInstance
-from row.models.member import Member
-from row.models.role import RoleCategory, RoleType, Role, RoleInstance, RoleRecord
-from row.models.team import TeamCategory, TeamType, Team, TeamInstance, TeamRecord
