ba3944892819c1ceaa89cc13f16011934cd436ce
Nicholas Piano <nicholas.d.piano@gmail.com>
2018-01-15 15:02:59 +0000

    Move single logic

--- django_backend/rowbot/static/rowbot/js/app/interfaces/member/app.interfaces.member.single.js
+++ django_backend/rowbot/static/rowbot/js/app/interfaces/member/app.interfaces.member.single.js
@@ -42,6 +42,139 @@ App.interfaces.member.single = function () {
       }),
     ],
   }).then(function (_single) {
+
+    // vars
+    var _activation = _single.get('activation');
+    var _roles = _single.get('roles');
+
+    // single
+    _single.load = function () {
+      // loads a member id from the api buffer
+      var _member = api.active.Member;
+      return _.all([
+        _single.get('name').update({value: `${_member.first_name} ${_member.last_name}`}),
+        _single.get('email').update({value: _member.email}),
+        _activation.load({activated: _member.is_activated}),
+        _roles.data.load.main(),
+      ]);
+    }
+    _single.setStates([
+      ui._state('members', {
+        children: [
+          ui._state('single', {
+            fn: {
+              before: function (_this) {
+                return _this.removeClass('hidden');
+              },
+            },
+          }),
+          ui._state('new', {
+            fn: {
+              before: function (_this) {
+                return _this.setClasses('hidden');
+              },
+            },
+          }),
+        ],
+      }),
+    ]);
+
+    // activation
+    _activation.load = function (args) {
+      args = (args || {});
+      if (args.activated) {
+        return _activation.update({value: 'Activated'}).then(function () {
+          return _activation.setClasses('activated');
+        });
+      } else {
+        return _activation.update({value: 'Not yet activated'}).then(function () {
+          return _activation.removeClass('activated');
+        });
+      }
+    }
+
+    // roles
+    _roles.get('search').setStyle({'display': 'none'});
+    _roles.setTargets([
+      _roles._target('roles', {
+        exclusive: false,
+        _source: function (args) {
+          args = (args || {});
+          var _target = this;
+          return api.models.Role.objects.filter({force: args.force, data: (args.data || _target.data())});
+        },
+        data: function (args) {
+          args = (args || {});
+          return [
+            {
+              key: 'member__id',
+              value: api.active.Member._id,
+              q: 'AND',
+            },
+            {
+              key: 'model__club__id',
+              value: api.active.Club._id,
+              q: 'AND',
+            }
+          ];
+        },
+        normalise: function (_instance) {
+          return _.all([
+            _instance.related('model'),
+            _instance.related('member'),
+          ]).then(function (results) {
+            var [_model, _member] = results;
+            return _model.related('club').then(function (_club) {
+              return {
+                _id: _instance._id,
+                member: _member._id,
+                main: _model.verbose_name,
+                club: _club.name,
+              }
+            });
+          });
+        },
+        unit: function (name, args) {
+          return ui._component(name, {
+            style: {
+
+            },
+            children: [
+              Components.text('text', {
+                classes: ['notouch'],
+              }),
+            ],
+          }).then(function (_unit) {
+            _unit.isHidden = false;
+            _unit.update = function (_datum) {
+              _unit.datum = _datum;
+              return _unit.get('text').update({
+                title: _datum.normalised.main,
+                value: _datum.normalised.club,
+              }).then(function () {
+                return _unit;
+              });
+            }
+            _unit.hide = function () {
+              _unit.isHidden = true;
+              return _unit.setClasses('hidden');
+            }
+            _unit.show = function () {
+              _unit.isHidden = false;
+              return _unit.removeClass('hidden');
+            }
+
+            return _unit;
+          });
+        },
+      }),
+    ]);
+    _roles.data.display.filter.condition = function (_datum) { // override
+      return _.p(function () {
+        _datum.accepted = _datum.normalised.member === api.active.Member._id;
+      });
+    }
+
     return _single;
   })
 }
