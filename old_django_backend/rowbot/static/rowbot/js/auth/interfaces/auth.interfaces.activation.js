072f27422637f35d6eed8bddba0c82c4da465eb2
Nicholas Piano <nicholas.d.piano@gmail.com>
2018-07-18 20:12:55 +0100

    Remove old django backend

--- old_django_backend/rowbot/static/rowbot/js/auth/interfaces/auth.interfaces.activation.js
+++
@@ -1,9 +0,0 @@
-var Auth = (Auth || {});
-Auth.interfaces = (Auth.interfaces || {});
-Auth.interfaces.activation = function () {
-  return ui._component('activation', {
-    
-  }).then(function (_activation) {
-    return _activation;
-  });
-}
